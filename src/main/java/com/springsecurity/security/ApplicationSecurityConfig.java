a3c536b9e74a57e2e2512d8d295b49164cda9f4c
Muhammad Awais <muhammad.awais1@pk.nestle.com.pk>
2022-05-25 03:49:07 +0500

    3 DB AUTH

--- src/main/java/com/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,24 +1,20 @@
 package com.springsecurity.security;
 
+import com.springsecurity.auth.ApplicationUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
-import static com.springsecurity.security.ApplicationUserRole.*;
+import static com.springsecurity.security.ApplicationUserRole.STUDENT;
 
 @AllArgsConstructor
 @EnableWebSecurity
@@ -26,6 +22,7 @@ import static com.springsecurity.security.ApplicationUserRole.*;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -42,60 +39,50 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                .antMatchers(HttpMethod.GET,"/management/api/**")
-//      Get Actually not required because Order maters and most of the others are already checked
+//      Get Actually not required because "Order maters" and most of the others ( i.e. PUT,POST,DELETE) are already checked
 //                .hasAnyRole(ADMIN.name(),ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .formLogin()
-                    .loginPage("/login").permitAll()
-                    .defaultSuccessUrl("/courses",true)
-                    .usernameParameter("username")//Optional, default and changeable for form
-                    .passwordParameter("password")//Optional, default and changeable for form
+                .loginPage("/login").permitAll()
+                .defaultSuccessUrl("/courses", true)
+                .usernameParameter("username")//Optional, default and changeable for form
+                .passwordParameter("password")//Optional, default and changeable for form
                 .and()
                 .rememberMe()//Default 2 weeks and have separate cookie;
-                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-                    .key("something very secure as a key")
-                    .rememberMeParameter("remember-me")//Optional, default and changeable for form
+                .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                .key("something very secure as a key")
+                .rememberMeParameter("remember-me")//Optional, default and changeable for form
                 .and()
                 .logout()
-                    .logoutUrl("/logout")
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout","GET"))//For Best practice by Documentation (Should remove with CSRF enable because of POST request requirement)
-                    .clearAuthentication(true)
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID","remember-me")
-                    .logoutSuccessUrl("/login")
+                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))//For Best practice by Documentation (Should remove with CSRF enable because of POST request requirement)
+                // ^ https://docs.spring.io/spring-security/site/docs/4.2.12.RELEASE/apidocs/org/springframework/security/config/annotation/web/configurers/LogoutConfigurer.html
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login")
         ;
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails awaisUser = User.builder()
-                .username("awais")
-                .password(passwordEncoder.encode("pas"))
-//                .roles(STUDENT.name()) // ROLE_STUDENT
-                .authorities(STUDENT.getGrantedAuthorities())
-                .build();
-
-        UserDetails aliUser = User.builder()
-                .username("ali")
-                .password(passwordEncoder.encode("pas1"))
-//                .roles(ADMIN.name()) // ROLE_ADMIN
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-
-        UserDetails tomUser = User.builder()
-                .username("tom")
-                .password(passwordEncoder.encode("pas1"))
-//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-
-        return new InMemoryUserDetailsManager(
-                awaisUser,
-                aliUser,
-                tomUser
-        );
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+    }
+
+    /* Alternate for above two methods (Not by course but my self)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(applicationUserService).passwordEncoder(passwordEncoder);
+    }
+    */
 }
